e7de4f445d3b1e15acbcf2cd1fc3af6de7ee8872
Rebecca Michel <rebeccamichel1266@gmail.com>
2025-08-06 04:03:25 +0000

    Save current workflow before pulling updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm
+from forms import RegistrationForm, ProfileForm
+from flask import request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '31cf3dbee0da6ac8e22e3a06e26263b7q'  
@@ -13,8 +14,30 @@ def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash("Signup successful!", "success")
-        return redirect(url_for('home'))  # Redirect later to home or dashboard
+        #return redirect(url_for('home'))  # Redirect later to home or dashboard
+        return redirect(url_for('create_profile')) # now redirect to create profile page
+
     return render_template('signup.html', form=form)
+
+@app.route('/create_profile', methods=['GET', 'POST'])
+def create_profile():
+    form = ProfileForm()
+    if form.validate_on_submit():
+        flash("Profile created successfully!", "success")
+        return redirect(url_for('home'))
+    return render_template('create_profile.html', form=form)
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")  
+
+@app.route("/events")
+def events():
+    return render_template("events.html")  
+
+@app.route("/user_profile")
+def user_profile():
+    return render_template("user_profile_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
+from flask_wtf.file import FileField, FileAllowed
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Full Name',
@@ -11,3 +16,12 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+class ProfileForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=13, max=120)])
+    school = StringField('School', validators=[DataRequired()])
+    ice_breaker = TextAreaField("Ice Breaker Question", validators=[DataRequired()])
+    interests = TextAreaField("Interests", validators=[DataRequired()])
+    picture = FileField("Upload a Picture", validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField("Create Profile")
